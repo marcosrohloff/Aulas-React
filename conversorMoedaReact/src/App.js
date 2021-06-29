8e343679138f3405512e1cd6dd73a2477388d223
Marcos Fernando <marcos.fersh@gmail.com>
2021-06-29 12:58:02 -0300

    ConversorMoeda
    ConversorMoeda

---
+++ conversorMoedaReact/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Conversor from "./components/Conversor"
+
+function App() {
+  return (
+    <div className="App">
+      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+    </div>
+  );
+}
+
+export default App;
